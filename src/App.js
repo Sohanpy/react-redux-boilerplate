e89e8122f75001b40b7c14488ce2dc161e91c5b5
Sohanpy <sohanmock@gmail.com>
2019-09-08 19:46:23 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Hello World</h1>
+          <p class="lead">
+            This is your home.write your ideas is vs code and see the outcome
+            here B|
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { reducer as formReducers } from "redux-form";
+
+export default combineReducers({
+  dummy: () => 5
+});
